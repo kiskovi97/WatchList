799e71984577baca6711afb323b74e3b6be3e1c3
Gergo Kovacs <kiskovi97@gmail.com>
2025-09-15 14:30:33 +0200

    fix base

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,12 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// Use Node's crypto directly
-import { createHash } from 'node:crypto'
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue({
-      // force Vue plugin to use Node crypto
-      template: {
-        compilerOptions: {},
-        transformAssetUrls: {},
-      },
-      // monkey patch getCacheKey to avoid global.crypto
-      // @ts-expect-error private API
-      __vuePluginOptions: {
-        getCacheKey: (src: string) => createHash('sha256').update(src).digest('hex'),
-      },
-    }),
-    vueJsx(),
-    vueDevTools(),
-  ],
+  base: '/WatchList/', // 👈 important for GitHub Pages
+  plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
