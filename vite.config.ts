31fbd8880f69364b21f7728e2b6006729ec4bbba
Gergo Kovacs <kiskovi97@gmail.com>
2025-09-15 14:19:58 +0200

    fix node crash probably

--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,16 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+import { createRequire } from 'module'
+const require = createRequire(import.meta.url)
+;(global as any).crypto = require('crypto')
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
